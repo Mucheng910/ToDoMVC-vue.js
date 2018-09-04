8d136232e9f4d4b17b1b2211e7412ceba50b4ba5
Muchenng910 <3544306302@qq.com>
2018-09-04 22:35:12 +0800

    todo-vue

---
+++ js/app.js
@@ -0,0 +1,60 @@
+(function (window) {
+	'use strict';
+	var todos=[];
+
+	console.log(todos);
+	//noinspection JSAnnotator
+
+	var app=new Vue({
+		el:"#app",
+		data:{
+			todos: todos,
+			text: '',
+			isCompleted: false,
+			editing: true
+		},
+		filters:{
+			all:function () {
+			}
+
+		},
+		methods:{
+			f:function () {
+				window.alert("this.text")
+			},
+			add:function () {
+				if(this.text.trim().length===0){
+					return
+				}
+				this.todos.push({
+					text:this.text,
+					Completed:false
+				});
+				this.text=""
+			},
+			del:function (text) {
+				var todoIndex;
+				this.todos.find(function (todo,index) {
+					if(todo.text===text){
+						todoIndex=index
+					}
+				});
+				this.todos.splice(todoIndex,1)
+			},
+			edit:function () {
+				this.$nextTick(function () {
+					this.input.focus()
+				})
+			}
+		}
+
+	});
+
+
+
+
+	// Your starting point. Enjoy the ride!
+
+
+
+})(window);
